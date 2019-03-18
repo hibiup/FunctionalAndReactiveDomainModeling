b76f4e53edce7be49caa19326260117c71fcdc95
326487162 <Welcome@661>
2019-03-18 15:50:02 -0400

    Add repository

--- src/main/scala/com.hibiup/Example_7_Repository.scala
+++ src/main/scala/com.hibiup/Example_7_Repository.scala
@@ -12,15 +12,15 @@ object Example_7_Repository extends App {
     val today = Calendar.getInstance.getTime
 
     /*********************************
-     * 以下 1~3　属于代数定义
+     * 1）定义代数抽象
      * */
-    /** １）基本数据类型　*/
+    /** 1.1) 基本数据类型　*/
     type Amount = BigDecimal
     case class Balance(amount: Amount = 0)
     case class Account(no: String, name: String, dateOfOpening: Date = today, dateOfClosing: Option[Date] = None,
                        balance: Balance = Balance(0))
 
-    /** ２）Repository */
+    /** 1.2) Repository */
     trait Repository[A, IdType] {
         def query(id: IdType): Try[Option[A]]
         def store(a: A): Try[A]
@@ -43,9 +43,9 @@ object Example_7_Repository extends App {
         def query(openedOn: Date): Try[Seq[Account]] = Success(repo.values.filter(_.dateOfOpening == openedOn).toSeq)
     }
 
-    /** ３）Service */
+    /** 1.3) Service */
     trait AccountService[Account, Amount, Balance] {
-        /** 返回 AccountRepository => Try[Account] */
+        /** 返回 Function1[AccountRepository, Try[Account]] Lambda 表达式 */
         def open(no: String, name: String, openingDate: Option[Date]): AccountRepository => Try[Account]
         def close(no: String, closeDate: Option[Date]): AccountRepository => Try[Account]
         def debit(no: String, amount: Amount): AccountRepository => Try[Account]
@@ -53,11 +53,121 @@ object Example_7_Repository extends App {
         def balance(no: String): AccountRepository => Try[Balance]
     }
 
+
     /*********************************
-      * 以下第 4 属于代数的实现
+      * 2) 通过 for-comprehension 以实现函数式组合调用
+      */
+    /** 2.1）函数式组合调用 */
+    // 获得 Service 的合约接口 (在编写组合的时候，可以没有实现，函数式编程是面向合约的，并不会因为没有函数体而失败)，
+    // 实现可以被推迟到调用组合之前（第4步）再提供。
+    import AccountService._
+
+    // for-comprehension 要求结果(包括中间结果)必须是 Monad（具有 map 和 flatMap），如果不是，要提供隐式转换。
+    import Implicit._
+
+    // 函数组合
+    def op(no: String) = for {
+        _ <- credit(no, BigDecimal(100))   // 结果被更新到Repository: { "1" -> Account("1", Balance(100)) }，返回 Success|Failure
+        _ <- credit(no, BigDecimal(300))   // 结果被更新到Repository: { "1" -> Account("1", Balance(400)) }，返回 Success|Failure
+        _ <- debit(no, BigDecimal(160))    // 结果被更新到Repository: { "1" -> Account("1", Balance(240)) }，返回 Success|Failure
+        b <- balance(no)                   // 返回结果： Success(Balance(240))，或　Failure(...)
+    } yield b
+
+
+    /*********************************
+      * 3）为了实现以上代数的组合调用，将一般函数 AccountRepository => Try[Account] 提升到 Monad　以支持从 for-comprehension　
+      * 中返回。（可以用 Cats 简化以下代码．）
+      */
+    /** 3.1) 定义 Monad */
+    object Syntax {
+        //* 定义 Monad */
+        trait Functor[F[_]] {
+            def map[A, B](a: F[A])(f: A => B): F[B]
+        }
+
+        object Functor {
+            def apply[F[_] : Functor]: Functor[F] = implicitly[Functor[F]]
+
+            implicit def ListFunctor: Functor[List] = new Functor[List] {
+                def map[A, B](a: List[A])(f: A => B): List[B] = a map f
+            }
+
+            implicit def OptionFunctor: Functor[Option] = new Functor[Option] {
+                def map[A, B](a: Option[A])(f: A => B): Option[B] = a map f
+            }
+
+            implicit def Tuple2Functor[A1]: Functor[({type f[x] = (A1, x)})#f] = new Functor[({type f[x] = (A1, x)})#f] {
+                def map[A, B](a: (A1, A))(f: A => B): (A1, B) = (a._1, f(a._2))
+            }
+
+            implicit def Function1Functor[A1]: Functor[({type f[x] = A1 => x})#f] = new Functor[({type f[x] = A1 => x})#f] {
+                def map[A, B](fa: A1 => A)(f: A => B): A1 => B = fa andThen f
+            }
+
+        }
+
+        trait Monad[F[_]] extends Functor[F] {
+            def unit[A](a: => A): F[A]
+            def flatMap[A, B](fa: F[A])(f: A => F[B]): F[B] = join(map(fa)(f))
+            def map[A, B](ma: F[A])(f: A => B): F[B] = flatMap(ma)(a => unit(f(a)))
+            def map2[A, B, C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] = flatMap(ma)(a => map(mb)(b => f(a, b)))
+            def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
+            def sequence[A](lma: List[F[A]]): F[List[A]] =
+                lma.foldRight(unit(List[A]())) { (ma, mla) => println(s"from monad $ma"); map2(ma, mla)(_ :: _) }
+            def traverse[A, B](la: List[A])(f: A => F[B]): F[List[B]] =
+                la.foldRight(unit(List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
+
+        }
+
+        object Monad {
+            def apply[F[_] : Monad]: Monad[F] = implicitly[Monad[F]]
+
+            implicit val optionMonad: Monad[Option] = new Monad[Option] {
+                def unit[A](a: => A) = Some(a)
+                override def flatMap[A, B](ma: Option[A])(f: A => Option[B]): Option[B] = ma flatMap f
+            }
+            implicit val listMonad: Monad[List] = new Monad[List] {
+                def unit[A](a: => A) = List(a)
+                override def flatMap[A, B](ma: List[A])(f: A => List[B]): List[B] = ma flatMap f
+            }
+
+            implicit def function1Monad[A1]: Monad[({type f[x] = A1 => x})#f] = new Monad[({type f[x] = A1 => x})#f] {
+                def unit[A](a: => A): A1 => A = (_: A1) => a
+                override def flatMap[A, B](r: A1 => A)(f: A => A1 => B): A1 => B = (t: A1) => f(r(t))(t)
+            }
+        }
+    }
+
+    /** 3.2) 为 for-comprehension 中的返回值　AccountRepository => Try[Account] 隐式添加 map　和 flatMap 方法． */
+    object Implicit {
+        import Syntax._
+
+        implicit class FunctorSyntax[F[_] : Functor, A](a: F[A]) {
+            def map[B](f: A => B): F[B] = Functor[F].map(a)(f)
+        }
+
+        implicit class Function1FunctorSyntax[A1, A](a: A1 => A) {
+            def map[B](f: A => B): A1 => B = Functor[({type f[x] = A1 => x})#f].map(a)(f)
+        }
+
+        implicit class MonadSyntax[M[_] : Monad, A](a: M[A]) {
+            def unit[A](a: => A): M[A] = Monad[M].unit(a)
+            def flatMap[B](f: A => M[B]): M[B] = Monad[M].flatMap(a)(f)
+        }
+
+        implicit class Function1MonadSyntax[A1, A](a: A1 => A) {
+            def unit[A](a: => A): A1 => A = Monad[({type f[x] = A1 => x})#f].unit(a)
+            def flatMap[B](f: A => A1 => B): A1 => B = Monad[({type f[x] = A1 => x})#f].flatMap(a)(f)
+        }
+
+    }
+
+    /*********************************
+      * 4）代数的实现
       * */
-    /** ４）Service 实现 */
+    /** 4.1）实现 Service  */
     case object AccountService extends AccountService[Account, Amount, Balance] {
+        /** 返回 Function1[AccountRepository, Try[Account]] Lambda 表达式 */
         override def open(no: String, name: String, openingDate: Option[Date]): AccountRepository => Try[Account] = (repo: AccountRepository) =>
             repo.query(no) match {
                 case Success(Some(a)) => Failure(new Exception(s"Already existing account with no $no"))
@@ -98,96 +208,8 @@ object Example_7_Repository extends App {
             (repo: AccountRepository) => repo.balance(no)
     }
 
-
     /*********************************
-      * 以下为了实现代数的组合调用，将一般函数 AccountRepository => Try[Account] 提升到 Monad　以支持从 for-comprehension　中返回
-      * 可以用 Cats 简化以下代码．
-      */
-    object Syntax {
-        //* 定义 Monad */
-        trait Functor[F[_]] {
-            def map[A, B](a: F[A])(f: A => B): F[B]
-        }
-
-        object Functor {
-            def apply[F[_]: Functor]: Functor[F] =  implicitly[Functor[F]]
-            implicit def ListFunctor: Functor[List] = new Functor[List] {
-                def map[A, B](a: List[A])(f: A => B): List[B] = a map f
-            }
-            implicit def OptionFunctor: Functor[Option] = new Functor[Option] {
-                def map[A, B](a: Option[A])(f: A => B): Option[B] = a map f
-            }
-            implicit def Tuple2Functor[A1]: Functor[({type f[x] = (A1, x)})#f] = new Functor[({type f[x] = (A1, x)})#f] {
-                def map[A, B](a: (A1, A))(f: A => B): (A1, B) = (a._1, f(a._2))
-            }
-            implicit def Function1Functor[A1]: Functor[({type f[x] = A1 => x})#f] = new Functor[({type f[x] = A1 => x})#f] {
-                def map[A, B](fa: A1 => A)(f: A => B): A1 => B = fa andThen f
-            }
-
-        }
-
-        trait Monad[F[_]] extends Functor[F] {
-            def unit[A](a: => A): F[A]
-            def flatMap[A,B](fa: F[A])(f: A => F[B]): F[B] =  join(map(fa)(f))
-            def map[A,B](ma: F[A])(f: A => B): F[B] =  flatMap(ma)(a => unit(f(a)))
-            def map2[A,B,C](ma: F[A], mb: F[B])(f: (A, B) => C): F[C] =  flatMap(ma)(a => map(mb)(b => f(a, b)))
-            def sequence[A](lma: List[F[A]]): F[List[A]] =
-                lma.foldRight(unit(List[A]())){(ma, mla) => println(s"from monad $ma"); map2(ma, mla)(_ :: _)}
-            def traverse[A,B](la: List[A])(f: A => F[B]): F[List[B]] =
-                la.foldRight(unit(List[B]()))((a, mlb) => map2(f(a), mlb)(_ :: _))
-            def join[A](mma: F[F[A]]): F[A] = flatMap(mma)(ma => ma)
-        }
-
-        object Monad {
-            def apply[F[_]: Monad]: Monad[F] = implicitly[Monad[F]]
-            implicit val optionMonad: Monad[Option] = new Monad[Option] {
-                def unit[A](a: => A) = Some(a)
-                override def flatMap[A,B](ma: Option[A])(f: A => Option[B]): Option[B] = ma flatMap f
-            }
-            implicit val listMonad: Monad[List] = new Monad[List] {
-                def unit[A](a: => A) = List(a)
-                override def flatMap[A,B](ma: List[A])(f: A => List[B]): List[B] = ma flatMap f
-            }
-            implicit def function1Monad[A1]: Monad[({type f[x] = A1 => x})#f] = new Monad[({type f[x] = A1 => x})#f] {
-                def unit[A](a: => A): A1 => A = (_: A1) => a
-                override def flatMap[A, B](r: A1 => A)(f: A => A1 => B): A1 => B = (t: A1) => f(r(t))(t)
-            }
-        }
-
-        /** 为 for-comprehension 中的返回值　AccountRepository => Try[Account] 隐式添加 map　和 flatMap 方法．*/
-        implicit class FunctorSyntax[F[_] : Functor, A](a: F[A]) {
-            def map[B](f: A => B): F[B] = Functor[F].map(a)(f)
-        }
-        implicit class Function1FunctorSyntax[A1, A](a: A1 => A) {
-            def map[B](f: A => B): A1 => B = Functor[({type f[x] = A1 => x})#f].map(a)(f)
-        }
-        implicit class MonadSyntax[M[_] : Monad, A](a: M[A]) {
-            def unit[A](a: => A): M[A] = Monad[M].unit(a)
-            def flatMap[B](f: A => M[B]): M[B] = Monad[M].flatMap(a)(f)
-        }
-        implicit class Function1MonadSyntax[A1, A](a: A1 => A) {
-            def unit[A](a: => A): A1 => A = Monad[({type f[x] = A1 => x})#f].unit(a)
-            def flatMap[B](f: A => A1 => B): A1 => B = Monad[({type f[x] = A1 => x})#f].flatMap(a)(f)
-        }
-
-    }
-
-    /*********************************
-      * 通过 for-comprehension 以实现函数式组合调用
-      */
-    /** ５）函数式组合调用 */
-    import Syntax._     // <- for-comprehension 将结果隐式转换成 Monad，因此它需要返回值具有 map 和 flatMap 方法.
-    import AccountService._
-    def op(no: String) = for {
-        _ <- credit(no, BigDecimal(100))   // 结果被更新到Repository: { "1" -> Account("1", Balance(100)) }，返回 Success|Failure
-        _ <- credit(no, BigDecimal(300))   // 结果被更新到Repository: { "1" -> Account("1", Balance(400)) }，返回 Success|Failure
-        _ <- debit(no, BigDecimal(160))    // 结果被更新到Repository: { "1" -> Account("1", Balance(240)) }，返回 Success|Failure
-        b <- balance(no)                   // 返回结果： Success(Balance(240))，或　Failure(...)
-    } yield b
-
-
-    /*********************************
-      * 调用函数组合执行代码
+      * 5) 调用函数组合执行代码
       * */
     val repo = new AccountRepository()   // 初始化存储库
 
