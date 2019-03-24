8a5eac52e106dde0108e5760aa62f3102f878ca7
Jeff <jeffwji@gmail.com>
2019-03-23 21:15:29 -0400

    Add simplify Functor

--- src/test/scala/com/hibiup/Example_9_Functor_test.scala
+++ src/test/scala/com/hibiup/Example_9_Functor_test.scala
@@ -5,29 +5,28 @@ import org.scalatest.FlatSpec
 class Example_9_Functor_test extends FlatSpec{
     "A Generic Functor" should "" in {
         /**
-          * 1) 设计阶段
+          * 1) Functor
           */
-        object MyTest {
+        object MyFunctor {
             // 定义 Functor 的界面
             trait Functor[F[_]] {
                 def map[A, B](a: F[A])(f: A => B): F[B]
             }
 
-            // 定义一个容器
-            case class MyClass[T](d: T)
-            
-            // 将 MyClass 隐式转换成 Functor
-            implicit class MyClassOps(my: MyClass[Int]) {
+            // 将 T 隐式转换成 Functor
+            implicit class MyClassOps[A, T[_]](my: T[A]) {
                 // 根据 Functor trait 的定义调取 map
-                def map[B](f: Int => B)(implicit functor: Functor[MyClass]): MyClass[B] = functor.map(my)(f)
+                def map[B](f: A => B)(implicit functor: Functor[T]): T[B] = functor.map(my)(f)
             }
         }
 
-
         /**
-          * 2）使用阶段
+          * 2）设计阶段
           */
-        import MyTest._
+        import MyFunctor._
+
+        // 定义一个容器
+        case class MyClass[T](d: T)
 
         // 实例化 MyClass 容器的 Functor，实现 map 函数体（每一种类型(MyClass)，都要实现一个）
         implicit val MyClassFunctor = new Functor[MyClass] {
@@ -38,7 +37,20 @@ class Example_9_Functor_test extends FlatSpec{
             }
         }
 
+        /** 3) 使用调用 */
         // 使用 MyClass Functor（此时需要 map 的函数体）
+        val my = MyClass[Int](1)
+
+        println(my.map("\"" + _.toString + "\""))
+        println(my.map(i => if (i>0) true else false))
+    }
+
+
+    "Simplify Functor" should "" in {
+        case class MyClass[T](d: T) {
+            def map[B](f: T => B): MyClass[B] = MyClass(f(d))
+        }
+
         val my = MyClass[Int](1)
 
         println(my.map("\"" + _.toString + "\""))
