ba38ec48a025f39fed242986df13cf34b875387d
326487162 <Welcome@661>
2019-03-27 16:19:02 -0400

    Add State Monad

--- src/main/scala/com.hibiup/Example_12_State_RandomRecordFinder.scala
+++ src/main/scala/com.hibiup/Example_12_State_RandomRecordFinder.scala
@@ -2,6 +2,8 @@ package com.hibiup
 
 import java.util.{Calendar, Date}
 
+import scalaz.MonadState
+
 import scala.util.{Failure, Success, Try}
 import collection.mutable.{Map => MMap}
 
@@ -51,14 +53,20 @@ object Example_12_State_RandomRecordFinder extends App {
         def query(openedOn: Date): Try[Seq[Account]] = Success(repo.values.filter(_.dateOfOpening == openedOn).toSeq)
     }
 
-    // 根据随机数捡取记录
-    final class RandomAccountFinder(rep: AccountRepository) {
-        val no: Int = scala.util.Random.nextInt(10)
-        def exists: Boolean = rep.query(no) match {
-            case Success(Some(a)) => true
-            case _ => false
-        }
+    // 数据捡取器接口
+    trait AccountFinder {
+        def exists: Boolean
     }
+
+    /** 生成 whileLoop State Monad 更合适的地方. 参考下面实现阶段的说明 */
+    /*def getWhileLoop[F[_], A <: AccountFinder](f: => A)(implicit m: MonadState[F, A]) = {
+        import m._
+        whileM_(
+            gets(!_.exists),
+            modify(_ => f)  // 直接内部改变 S,这也是为什么这个 State 没有执行函数
+        )
+    }*/
+
 
     /***********************************/
     /** 实现时 */
@@ -68,15 +76,30 @@ object Example_12_State_RandomRecordFinder extends App {
         1->Account(1, "John"))
     )
 
+    // 设计随机数据捡取器
+    final class RandomAccountFinder(rep: AccountRepository) extends AccountFinder {
+        val account = rep.query(scala.util.Random.nextInt(10))
+        def exists: Boolean = account match {
+            case Success(Some(a)) => true
+            case _ => false
+        }
+    }
+
     // 将 RandomAccountFinder 转换成 State Monad. 如果没有执行函数, State Monad 可以只接受 S 类型参数.
     // StateT是 monad 变换器, 它在这里做的并不重要, 只是帮助我们忽略第二个参数.
     import scalaz.StateT
-    val RandomAccountFinderState = StateT.stateMonad[RandomAccountFinder]
+    implicit val RandomAccountFinderState =   // 如果 whileLoop 不是设计时需求的, 并非必须 implicit
+        StateT.stateMonad[RandomAccountFinder]
 
+    /** whileM_　是所有的 Monad 都具有的一个隐式植入函数, 它返回一个 State Monad.
+      *
+      * 注:这个 特定的 while 应该改到建模阶段. (参考建模阶段的 getWhileLoop 函数 ). 用以下指令替换
+      * */
     import RandomAccountFinderState.{whileM_, gets, modify}
-    /** whileM_　是 Monad 的一个隐式植入函数, 它返回一个新的 State Monad. */
-    val whileLoop = whileM_(     // 只要 !_.exists==true 就会一直执行，也就是只要找不到就循环到找到为止。返回 State Monad: whileM_
-        /** gets 是 MonadState 的方法, 这里用于得到新 State 的 S 参数 */
+    // val whileLoop = getWhileLoop(new RandomAccountFinder(r))
+    val whileLoop = whileM_(     // 返回 State Monad: whileM_
+        /** gets 是 MonadState 的方法, 这里用于得到新 State 的 S 参数
+          * 只要 !_.exists==true 就会一直执行，也就是只要找不到就循环到找到为止。 */
         gets(!_.exists),
         /** 这个也是 MonadState.modify. 和前例中的 StateFunctions.modify 不同, 这里用于得到新 State 的 A 参数 */
         modify(_ => new RandomAccountFinder(r))  // 直接内部改变 S,这也是为什么这个 State 没有执行函数
@@ -84,5 +107,5 @@ object Example_12_State_RandomRecordFinder extends App {
 
     val gen = new RandomAccountFinder(r)    // 生成初始状态
     val rnd = whileLoop exec gen            // 传入初始状态给 RandomAccountFinderState 开始执行查找
-    println(rnd.no)
+    println(rnd.account)
 }
