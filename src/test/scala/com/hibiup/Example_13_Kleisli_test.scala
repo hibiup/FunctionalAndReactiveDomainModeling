86716f97ceb0e881e5bfde615e8e726d6170f588
326487162 <Welcome@661>
2019-04-01 13:49:05 -0400

    Add Kleisli Example

--- src/test/scala/com/hibiup/Example_13_Kleisli_test.scala
+++ src/test/scala/com/hibiup/Example_13_Kleisli_test.scala
@@ -7,20 +7,49 @@ class Example_13_Kleisli_test extends FlatSpec{
         import scalaz._
         import Scalaz._
 
-        /** 定义时无法直接基于 FlatMap 组合抽象.*/
-        def f(i: Int) = List(i, 2 * i)
-        def g(j: Int) = List(j * 2, j * 4)
 
-        // 在实现时带入值进行组合
-        val res1 = f(10).flatMap(g)
-        assert(res1 ==  List(20, 40, 40, 80))
+        /** for-comprehension 帮助在定义时基于 Monad 实现组合抽象.*/
+        trait test1 {
+            def f1(i: Int): List[Int]
+            def g1(j: Int): List[Int]
 
-        /** 相比而言，Kleisli 在更高的层次上抽象了运算,可以允许我们在定义时就进行组合 */
-        import scalaz._
-        import Scalaz._
-        val resK = Kleisli(f) >=> Kleisli(g)   // 在设计时实现组合. 也就是说 Kleisli 将 for-comprehension 的工作移到设计时完成
+            // 基于 flatMap 的组合, 需要仔细考虑组合的方式,容易出错.
+            def res(i:Int) = for{
+                f <- f1(i)
+                g <- g1(f)
+            } yield(g)
+        }
 
-        // 实现时只需要带入值即可。
-        assert(resK(10) ==  List(20, 40, 40, 80))
+        // 在实现时带入值进行计算
+        val t1 = new test1() {
+            override def f1(i: Int): List[Int] = List(i, 2 * i)
+            override def g1(j: Int): List[Int] = List(j * 2, j * 4)
+        }
+        import t1._
+        assert(res(10) ==  List(20, 40, 40, 80))
+
+
+        /** 基于 Kleisli 的组合 */
+        trait test2 {
+            import scalaz._
+            import Scalaz._
+
+            def f(i: Int): List[Int]
+            def g(j: Int): List[Int]
+
+            // Kleisli 定义组合更直观, 表达性更好, 更灵活
+            val resK1 = Kleisli(f) >=> Kleisli(g)
+            val resK2 = Kleisli(f) <=< Kleisli(g)
+        }
+
+        // 在实现时带入值进行计算
+        val t2 = new test2() {
+            override def f(i: Int): List[Int] = List(i, 2 * i)
+            override def g(j: Int): List[Int] = List(j * 2, j * 4)
+        }
+
+        import t2._
+        assert(resK1(10) ==  List(20, 40, 40, 80))
+        assert(resK2(10) ==  List(20, 40, 40, 80))
     }
 }
