51078ed0ffd183260640ca5886de6146ed30fa25
326487162 <Welcome@661>
2019-04-08 12:05:32 -0400

    Update transformer example

--- src/main/scala/com.hibiup/Example_13_Kleisli_Transformer.scala
+++ src/main/scala/com.hibiup/Example_13_Kleisli_Transformer.scala
@@ -18,20 +18,24 @@ package example_18 {
 
         object DesignInterpreter extends Design {
             override def i2f: Kleisli[Valid, Int, BigDecimal] = Kleisli { i =>
+                println(s"[Thread-${Thread.currentThread().getId}] - i2f")
                 if (i >= 0) BigDecimal(i).right else (new RuntimeException("Input is smaller then 0")).left
             }
 
             override def f2s: Kleisli[Valid, BigDecimal, String] = Kleisli { f =>
+                println(s"[Thread-${Thread.currentThread().getId}] - f2s")
                 f.toString().right
             }
         }
 
         import DesignInterpreter._
 
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
         comp(-1) match {
             case \/-(s) => println(s"Finally we got: ${s}")
             case -\/(e) => println(e.getMessage)
         }
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
     }
 
     /**
@@ -54,17 +58,28 @@ package example_18 {
         object DesignInterpreter extends Design {
             override def i2f: Kleisli[Future, Int, Valid[BigDecimal]] = Kleisli { i =>
                 Future {
+                    println(s"[Thread-${Thread.currentThread().getId}] - i2f")
                     if (i >= 0) (BigDecimal(i)).right else (new RuntimeException("Input is smaller then 0")).left
                 }
             }
 
-            /** 不使用 Transformer 则需要将 Valid 解出，然后再按 Future -> Valid 打包回去 */
-            override def f2s: Kleisli[Future, Valid[BigDecimal], Valid[String]] = Kleisli {
-                // 解出 Either
-                case \/-(s) => Future{   // 重新打包成 Future[Either]
-                    s.toString.right
+            /** 不使用 Transformer 则需要将 f:Valid 解出，然后再按 Future -> Valid 打包回去．
+              *
+              * 不仅需要拆包，并且会导致以下 Future 必定会被执行，即便前一步骤返回异常（因为不能提前判断）。
+              * */
+            override def f2s: Kleisli[Future, Valid[BigDecimal], Valid[String]] = Kleisli {f:Valid[_] =>
+                println(s"[Thread-${Thread.currentThread().getId}] - f2s in the same thread of i2f's Future")
+                // 解出 Either.
+                f match {
+                    case \/-(s) => Future { // 重新打包成 Future[Either]
+                        println(s"[Thread-${Thread.currentThread().getId}] - f2s in Future")
+                        s.toString.right
+                    }
+                    case -\/(e) => Future {
+                        println(s"[Thread-${Thread.currentThread().getId}] - f2s in Future <- 不应该执行")
+                        e.left
+                    }
                 }
-                case -\/(e) => Future{ e.left }
             }
         }
 
@@ -72,10 +87,20 @@ package example_18 {
         import scala.concurrent.Await
         import scala.concurrent.duration._
 
-        Await.result(comp(-1), 10 seconds)match {
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
+        Await.result(comp(2), 10 seconds)match {
             case \/-(s) => println(s"Finally we got: ${s}")
             case -\/(e) => println(e.getMessage)
         }
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
+
+        println(s"[Thread-${Thread.currentThread().getId}] - i2f 失败并不会提前终止 f2s")
+        Await.result(comp(-2), 10 seconds)match {
+            case \/-(s) => println(s"Finally we got: ${s}")
+            case -\/(e) => println(e.getMessage)
+        }
+
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
     }
 
     /**
@@ -97,25 +122,37 @@ package example_18 {
             def i2f: Kleisli[Valid, Int, BigDecimal]
             def f2s: Kleisli[Valid, BigDecimal, String]
             def comp: Kleisli[Valid, Int, String] = i2f andThen f2s
+
+            /** 与 Kleisli 不同, for-comprehension 属于实现时手段, 它会暴露参数传递的过程 */
+            def compFor: Int => Valid[String] = i => for {
+                f <- i2f(i)
+                s <- f2s(f)
+            } yield s
         }
 
         object DesignInterpreter extends Design {
-            /** 使用了 Transformer 实现的时候也需要一层一层按顺序构建，但是不需要对输入值 i（Valid） 进行解包，相比上一种方法等于
-              * 少了一步，降低了输入处理的复杂度。 */
-            override def i2f: Kleisli[Valid, Int, BigDecimal] = Kleisli { i =>  // 不必对 i 解包，直接就得到输入值。
-                // 打包回 EitherT[Future[Either]]
+            override def i2f: Kleisli[Valid, Int, BigDecimal] = Kleisli { i =>
+                println(s"[Thread-${Thread.currentThread().getId}] - i2f")
+                /** 返回的时候需要一层一层按顺序打包回 EitherT[Future[Either]]　*/
                 EitherT {
                     Future {
-                        if (i >= 0) BigDecimal(i).right     // 虽然输入简化了，但是返回值还是要打包成 Transformer 支持的类型
+                        println(s"[Thread-${Thread.currentThread().getId}] - i2f in Future")
+                        if (i >= 0) BigDecimal(i).right
                         else (new RuntimeException("Input is smaller then 0")).left
                     }
                 }
             }
 
-            override def f2s: Kleisli[Valid, BigDecimal, String] = Kleisli { f =>
+            /** 直接得到 f:BigDecimal, 相比上一种方法不需要对输入值进行解包，降低了输入处理的复杂度。
+              *
+              * 并且因为提前拆包，因此如果前一步骤返回异常，以下过程得以避免执行.
+              * */
+            override def f2s: Kleisli[Valid, BigDecimal, String] = Kleisli { f:BigDecimal =>     // 不必对 f 解包，直接就得到输入值。
+                println(s"[Thread-${Thread.currentThread().getId}] - f2s in the same thread of i2f's Future  <- 如果前一步骤失败, 将被避免执行")
                 EitherT {
                     Future {
-                        f.toString.right
+                        println(s"[Thread-${Thread.currentThread().getId}] - f2s in Future")
+                        f.toString.right     // 虽然输入简化了，但是返回值还是要打包成 Transformer 支持的类型
                     }
                 }
             }
@@ -125,7 +162,32 @@ package example_18 {
         import scala.concurrent.Await
         import scala.concurrent.duration._
 
+        println(s"[Thread-${Thread.currentThread().getId}] - main === 测试 Kleisli ===")
         comp(2) match {
+            case EitherT(f) => Await.result(f.map {
+                case \/-(s) => println(s"Finally we got: ${s}")
+                case -\/(e) => println(e.getMessage)
+            }, 10 seconds)
+        }
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
+
+        println(s"[Thread-${Thread.currentThread().getId}] - i2f 失败将避免执行 f2s")
+        comp(-2) match {
+            case EitherT(f) => Await.result(f.map {
+                case \/-(s) => println(s"Finally we got: ${s}")
+                case -\/(e) => println(e.getMessage)
+            }, 10 seconds)
+        }
+
+        println(s"[Thread-${Thread.currentThread().getId}] main === 测试 for-comprehension ===")
+        compFor(2) match {
+            case EitherT(f) => Await.result(f.map {
+                case \/-(s) => println(s"Finally we got: ${s}")
+                case -\/(e) => println(e.getMessage)
+            }, 10 seconds)
+        }
+        println(s"[Thread-${Thread.currentThread().getId}] - main")
+        compFor(-2) match {
             case EitherT(f) => Await.result(f.map {
                 case \/-(s) => println(s"Finally we got: ${s}")
                 case -\/(e) => println(e.getMessage)
@@ -145,15 +207,16 @@ package example_18 {
         type FutureValid[A] = Future[Valid[A]]   // 将 Future 作为返回类型
 
         trait Design {
-            def i2f: Kleisli[EitherT, Int, FutureValid[BigDecimal]]
-            def f2s: Kleisli[EitherT, FutureValid[BigDecimal], FutureValid[String]]
+            //def i2f: Kleisli[EitherT, Int, FutureValid[BigDecimal]]
+            //def f2s: Kleisli[EitherT, FutureValid[BigDecimal], FutureValid[String]]
+
             /** 没有缺省的 Bind[EitherT], 无法实现 Compose */
             // def comp: Kleisli[EitherT, Int, FutureValid[String]] = i2f andThen f2s
         }
 
-        object DesignInterpreter extends Design {
+        /*object DesignInterpreter extends Design {
             override def i2f: Kleisli[EitherT, Int, FutureValid[BigDecimal]] = ???
             override def f2s: Kleisli[EitherT, FutureValid[BigDecimal], FutureValid[String]] = ???
-        }
+        }*/
     }
 }
